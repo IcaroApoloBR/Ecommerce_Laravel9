daee569d75e3489d7eeb3cc65352018f17155895
Icaro Apolo <apoloraci@gmail.com>
2022-06-25 12:12:28 -0300

    style(template): add views with template tailblocks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/produtos', [ProductController::class, 'show']);
+
+Route::get('/admin/produtos', [AdminProductController::class, 'index']);
+Route::get('/admin/editar', [AdminProductController::class, 'edit']);
