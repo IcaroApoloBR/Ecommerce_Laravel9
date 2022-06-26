a9a500a51d9a9f4f51f5686eb92a941266fe003f
Icaro Apolo <apoloraci@gmail.com>
2022-06-26 11:51:55 -0300

    feat(createProduct): page create product done, store in database with path url image too

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class AdminProductController extends Controller
 {
@@ -33,7 +35,26 @@ class AdminProductController extends Controller
     }
 
     // Receives a request to create the product POST
-    public function store()
+    public function store(Request $request)
     {
+        $input = $request->validate([
+            'name' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable',
+        ]);
+
+        $input['slug'] = Str::slug($input['name']);
+
+        if (!empty($input['cover']) && $input['cover']->isValid()) {
+            $file = $input['cover'];
+            $path = $file->store('products');
+            $input['cover'] = $path;
+        }
+
+        Product::create($input);
+
+        return Redirect::route('admin.products');
     }
 }
