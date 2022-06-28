6adc208966ee20fb991019f4f77afd9a61fd35d3
Icaro Apolo <apoloraci@gmail.com>
2022-06-28 19:11:52 -0300

    fix(layouts): ajust layouts default infos

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>E-Commerce</title>
+    <title> Laravel E-Commerce</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -43,15 +43,14 @@
             <div class="container px-5 py-8 flex flex-wrap mx-auto items-center">
                 <div class="flex md:flex-nowrap flex-wrap justify-center items-end md:justify-start">
                     <div class="relative sm:w-64 w-40 sm:mr-4 mr-2">
-                        <label for="footer-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
+                        <label for="footer-field" class="leading-7 text-sm text-gray-600">Informe seu e-mail</label>
                         <input type="text" id="footer-field" name="footer-field"
                             class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-rose-200 focus:border-rose-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                     </div>
                     <button
-                        class="inline-flex text-white bg-rose-500 border-0 py-2 px-6 focus:outline-none hover:bg-rose-600 rounded">Button</button>
-                    <p class="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones
-                        fanny pack
-                        <br class="lg:block hidden">waistcoat green juice
+                        class="inline-flex text-white bg-rose-500 border-0 py-2 px-6 focus:outline-none hover:bg-rose-600 rounded">Enviar</button>
+                    <p class="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Deseja receber notificações
+                        <br class="lg:block hidden">sobre seus produtos favoritos ?
                     </p>
                 </div>
                 <span class="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
@@ -91,13 +90,12 @@
         </div>
         <div class="bg-gray-100">
             <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                    <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" target="_blank"
-                        rel="noopener noreferrer">@knyttneve</a>
+                <p class="text-gray-500 text-sm text-center sm:text-left">© 2022 Laravel com Tailwind — GitHub
+                    <a href="https://github.com/IcaroApoloBR/Ecommerce_Laravel9" class="text-gray-600 ml-1" target="_blank"
+                        rel="noopener noreferrer">@IcaroApoloBR</a>
                 </p>
                 <span
-                    class="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enamel
-                    pin tousled raclette tacos irony</span>
+                    class="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Curso Laravel 9 com o professor Tiago Matos</span>
             </div>
         </div>
     </footer>
